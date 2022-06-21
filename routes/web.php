2c446f4f33c3fa6db55eb7f2f81a78bec1a78ffd
Chelsea <makanayam749@yahoo.com>
2022-06-21 22:26:47 +0700

    c

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
