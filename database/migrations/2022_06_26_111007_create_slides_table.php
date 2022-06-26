3770c644604de8e532bbbceb98a7a9f4e3e203cf
Chelsea <makanayam749@yahoo.com>
2022-06-26 18:17:50 +0700

    SLides table

--- database/migrations/2022_06_26_111007_create_slides_table.php
+++ database/migrations/2022_06_26_111007_create_slides_table.php
@@ -15,6 +15,7 @@ class CreateSlidesTable extends Migration
     {
         Schema::create('slides', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->foreignId('game_id');
             $table->timestamps();
         });
